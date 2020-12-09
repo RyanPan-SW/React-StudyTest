bfae436d70660de9f2067630315a9b9024f6e02a
潘世文 <panshiwen@panshiwendeMacBook-Pro.local>
2020-12-09 15:47:45 +0800

    高阶组件,属性代理

--- src/index.js
+++ src/index.js
@@ -2,48 +2,39 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
 /**
- * HOC：反向继承
- * 应用场景： 开发很多button组件，都有一个共同的特点，初始值为0，每次点击之后增加1
- * 反向继承两大作用：
- *  1. 渲染劫持、
- *  2. 操作state（有自己的属性、状态）
- *  注意： 继承的state、props也会被覆盖
+ * HOC：属性代理
  */
-class Button extends Component {
-  componentDidMount() {
-    console.log("1. Button componentDidMount");
-  }
 
-  render() {
-    console.log("1.Button render");
-    return <button name={this.props.name}></button>;
-  }
-}
-
-const Wrapper = OldComponent => {
-  return class extends OldComponent {
-    state = { number: 0 };
-    componentDidMount() {
-      console.log("2. wrapper componentDidMount");
-      // 如果项调用继承的 componentDidMount,可以手动调用
-      super.componentDidMount();
-    }
-
-    handleClick = () => {
-      this.setState({ number: this.state.number + 1 });
-    };
-
+const loading = message => OldComponent => {
+  return class extends Component {
     render() {
-      console.log("2. wrapper render");
-      const renderElement = super.render();
-      const newProps = { ...renderElement.props, onClick: this.handleClick };
-      // 把渲染的内容改变了
-      const newElement = React.cloneElement(renderElement, newProps, this.state.number);
-      return newElement;
+      const state = {
+        show: () => {
+          let loadmodal = document.createElement("div");
+          loadmodal.innerHTML = `<p id="loading" style="position:fixed;top:100px;z-index:10;background-color:#00000034">${message}</p>`;
+          document.body.appendChild(loadmodal);
+        },
+        hide: () => {
+          document.getElementById('loading').remove()
+        },
+      };
+      return (
+          <OldComponent {...this.props} {...state} />
+      );
     }
   };
 };
 
-const NewButton = Wrapper(Button);
+let LoadingHello = loading("正在加载...")(Hello);
 
-ReactDOM.render(<NewButton name={"zhangSan"} />, document.getElementById("root"));
+function Hello(props) {
+  return (
+    <div>
+      <p>Hello</p>
+      <button onClick={props.show}>show</button>
+      <button onClick={props.hide}>hide</button>
+    </div>
+  );
+}
+
+ReactDOM.render(<LoadingHello />, document.getElementById("root"));
