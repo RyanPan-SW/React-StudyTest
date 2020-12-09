54944868e5a0bb6c2b70ecc43df59d1af3363acb
潘世文 <panshiwen@panshiwendeMacBook-Pro.local>
2020-12-09 15:20:18 +0800

    高阶组件,反向继承

--- src/index.js
+++ src/index.js
@@ -1,74 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
-/* class ClassComponent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { x: 0, y: 0, a: 1 };
+/**
+ * HOC：反向继承
+ * 应用场景： 开发很多button组件，都有一个共同的特点，初始值为0，每次点击之后增加1
+ * 反向继承两大作用：
+ *  1. 渲染劫持、
+ *  2. 操作state（有自己的属性、状态）
+ *  注意： 继承的state、props也会被覆盖
+ */
+class Button extends Component {
+  componentDidMount() {
+    console.log("1. Button componentDidMount");
   }
 
-  onMouseMove = event => {
-    this.setState({ x: event.clientX, y: event.clientY });
-  };
-
   render() {
-    const { x, y } = this.state;
-    return (
-      <div onMouseMove={this.onMouseMove} style={{ width: 500, height: 500 }}>
-        {this.props.render({ x, y })}
-      </div>
-    );
+    console.log("1.Button render");
+    return <button name={this.props.name}></button>;
   }
 }
 
-ReactDOM.render(
-  <ClassComponent
-    render={value => {
-      console.log("object", value);
-      return (
-        <>
-          <div>mouse offset</div>
-          <p>
-            {value.x}: {value.y}
-          </p>
-        </>
-      );
-    }}
-  />,
-  document.getElementById("root")
-); */
-
-function WithHight(OldComponent) {
-  return class ClassComponent extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = { x: 0, y: 0, a: 1 };
+const Wrapper = OldComponent => {
+  return class extends OldComponent {
+    state = { number: 0 };
+    componentDidMount() {
+      console.log("2. wrapper componentDidMount");
+      // 如果项调用继承的 componentDidMount,可以手动调用
+      super.componentDidMount();
     }
 
-    onMouseMove = event => {
-      this.setState({ x: event.clientX, y: event.clientY });
+    handleClick = () => {
+      this.setState({ number: this.state.number + 1 });
     };
 
     render() {
-      return (
-        <div onMouseMove={this.onMouseMove} style={{ width: 500, height: 500 }}>
-          <OldComponent {...this.state} />
-        </div>
-      );
+      console.log("2. wrapper render");
+      const renderElement = super.render();
+      const newProps = { ...renderElement.props, onClick: this.handleClick };
+      // 把渲染的内容改变了
+      const newElement = React.cloneElement(renderElement, newProps, this.state.number);
+      return newElement;
     }
   };
-}
-function Show(props) {
-  return (
-    <>
-      <div>mouse offset</div>
-      <p>
-        {props.x}: {props.y}
-      </p>
-    </>
-  );
-}
+};
 
-let HightComponent = WithHight(Show);
+const NewButton = Wrapper(Button);
 
-ReactDOM.render(<HightComponent />, document.getElementById("root"));
+ReactDOM.render(<NewButton name={"zhangSan"} />, document.getElementById("root"));
