1f2a7938f69392fb60329a43fa953e2ddeb0ecea
潘世文 <panshiwen@panshiwendeMacBook-Pro.local>
2020-12-14 14:10:35 +0800

    docs: add vscode debug

--- src/index.js
+++ src/index.js
@@ -11,9 +11,12 @@ const loading = message => OldComponent => {
     render() {
       const state = {
         show: () => {
-          let loadmodal = document.createElement("div");
-          loadmodal.innerHTML = `<p id="loading" style="position:fixed;top:100px;z-index:10;background-color:#00000034">${message}</p>`;
-          document.body.appendChild(loadmodal);
+          let dom = document.getElementById("loading");
+          if (!dom) {
+            let loadmodal = document.createElement("div");
+            loadmodal.innerHTML = `<p id="loading" style="position:fixed;top:100px;z-index:10;background-color:#00000034">${message}</p>`;
+            document.body.appendChild(loadmodal);
+          }
         },
         hide: () => {
           document.getElementById("loading").remove();
@@ -36,7 +39,7 @@ function Hello(props) {
       <p>Hello</p>
       <button onClick={props.show}>show</button>
       <button onClick={props.hide}>hide</button>
-      <MyButton className={`aabbcc`} colors={'danger'} onClick={e => click(e)}>
+      <MyButton className={`aabbcc`} colors={"danger"} onClick={e => click(e)}>
         123
       </MyButton>
     </div>
